8499dce845d3e62727fc60bc50b222527835cc3a
sean cork <seantcork@gmaol.com>
2019-02-11 20:37:57 -0500

    added some getsockopt

--- code/Server.cpp
+++ code/Server.cpp
@@ -193,14 +193,14 @@ char *generate_response(string http_type, string filepath, string rootdir) {
         int n = response.length();
         char *char_array = new char[n+1];
         strcpy(char_array, response.c_str());
-        file.close()
+        file.close();
         return char_array;
     } else {
         // unable to read (permissions)
-        file.close()
+        file.close();
         return (char*)"403 Forbidden";
     }
-    file.close()
+    file.close();
     return (char*)"ERROR";
 }
 
@@ -269,7 +269,7 @@ int handle_request(char *msg, int socket, string rootdir) {
     bytes_sent = send(socket, reply, strlen(reply) ,0);
 
 	if(bytes_left == -1){
-		cerr << "Errror sendinfg file" << endl;
+		cerr << "Errror sending file" << endl;
 		return -1;
 	}
 	
@@ -307,8 +307,13 @@ void *new_connection(void *info) {
     int sock = args->arg2;
     
     int connection = 1;
-    
+    int timeout = 2;
 	while(connection){
+        
+        if(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)(&timeout), sizeof(timeout)) < 0){
+            cerr << "set sock options failing." << endl;
+            perror("socket optin failing");
+        }
 	    char req[MAXREQ] = {0};
 	    int n = read(sock, req, MAXURI);
 	    if (n < 0) {
@@ -392,6 +397,8 @@ int main(int argc, char** argv) {
         printf("error binding socket\n");
         return -1;
     }
+
+
     
     DEBUG_PRINT("opened and bound socket!\n");
     
