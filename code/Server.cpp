bca460b8561a0ffbd24b9b22de83f714192a75ec
sean cork <seantcork@gmaol.com>
2019-02-13 20:42:16 -0500

    added stuff

--- code/Server.cpp
+++ code/Server.cpp
@@ -401,7 +401,7 @@ void *new_connection(void *info) {
             if (n < 0) {
                 cerr << "error on read!/n" << endl;
             }
-            tokenize(req, rinfo);
+            tokenize_msg(req, rinfo);
             prints(rinfo);
         }
 	    
