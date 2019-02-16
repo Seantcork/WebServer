967440672c23d3eced731d359c5c1154f2cdc4c0
sean cork <seantcork@gmaol.com>
2019-02-15 22:14:39 -0500

    rootdir

--- code/Server.cpp
+++ code/Server.cpp
@@ -520,7 +520,7 @@ int main(int argc, char** argv) {
     
     //literals
     int c, err, portnum, pflag, rflag = 0;
-    char rootdir[200] = "";
+    string rootdir; 
     int sock_fd, new_sock, clientlen;
     struct sockaddr_in client_addr;
     //Parse command line
